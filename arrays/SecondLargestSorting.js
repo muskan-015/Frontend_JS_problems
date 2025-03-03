6d7bc9f7d4f00604dbad7824c2e9a78f6b66a70f
muskan-015 <muskanvds2004@gmail.com>
2025-03-03 21:18:30 +0530

    [Muskan] Add. Second largest sorting problem added

---
+++ arrays/SecondLargestSorting.js
@@ -0,0 +1,17 @@
+function findSecondLargestAndSmallestSorted() {
+    let numbers = [];
+    
+    for (let i = 0; i < 10; i++) {
+        numbers.push(Math.floor(100 + Math.random() * 900));
+    }
+
+    console.log("Generated Numbers:", numbers);
+
+    numbers.sort((a, b) => a - b);
+    console.log("Sorted Numbers:", numbers);
+
+    console.log("Second Smallest:", numbers[1]);
+    console.log("Second Largest:", numbers[numbers.length - 2]);
+}
+
+findSecondLargestAndSmallestSorted();
