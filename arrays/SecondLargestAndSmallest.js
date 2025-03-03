91b2e4b9ab1c07a2921db7f68974c3df4dea3a28
muskan-015 <muskanvds2004@gmail.com>
2025-03-03 21:17:28 +0530

    [Muskan] Add. Second largest and smallest problem added

---
+++ arrays/SecondLargestAndSmallest.js
@@ -0,0 +1,33 @@
+function findSecondLargestAndSmallest() {
+    let numbers = [];
+    
+    for (let i = 0; i < 10; i++) {
+        numbers.push(Math.floor(100 + Math.random() * 900));
+    }
+
+    console.log("Generated Numbers:", numbers);
+
+    let firstMax = -Infinity, secondMax = -Infinity;
+    let firstMin = Infinity, secondMin = Infinity;
+
+    for (let num of numbers) {
+        if (num > firstMax) {
+            secondMax = firstMax;
+            firstMax = num;
+        } else if (num > secondMax && num !== firstMax) {
+            secondMax = num;
+        }
+
+        if (num < firstMin) {
+            secondMin = firstMin;
+            firstMin = num;
+        } else if (num < secondMin && num !== firstMin) {
+            secondMin = num;
+        }
+    }
+
+    console.log("Second Largest:", secondMax);
+    console.log("Second Smallest:", secondMin);
+}
+
+findSecondLargestAndSmallest();
